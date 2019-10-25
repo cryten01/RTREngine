28030a5d6cc1ba2c65d0529a9059f12d5077dd80
C N <cryten101@gmail.com>
2019-10-25 18:03:45 +0200

    Camera - Added HSOS Camera

--- RTREngine/RTREngine/src/Utils.h
+++ RTREngine/RTREngine/src/Utils.h
@@ -9,15 +9,19 @@
 
 // GLEW
 #define GLEW_STATIC
-#include <GL\glew.h>
+#include <GL/glew.h>
 
 // GLFW (must be after GLEW)
 #include <GLFW/glfw3.h>
 
 // GLM
-#include <glm\glm.hpp>
+#include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+// HSOS
+#include <hsos\Camera.h>
+#include <hsos\Matrix.h>
+#include <hsos\Vector.h>
 
 // Custom error msg
 #define EXIT_WITH_ERROR(err) \

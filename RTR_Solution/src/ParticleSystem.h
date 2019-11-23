ff54cadce089057fa58355cdf42e70e9cfd0810c
C N <cryten101@gmail.com>
2019-11-23 18:24:17 +0100

    Fixed particle compute shader

--- RTR_Solution/src/ParticleSystem.h
+++ RTR_Solution/src/ParticleSystem.h
@@ -5,7 +5,7 @@
 
 struct Particle					// Note: assumes identical amounts for positionsTTL and velocity
 {
-	glm::vec4 positionsTTL;		// Includes TTL in order to avoid padding
+	glm::vec4 positionsTTL;		// Includes TTL in seconds in order to avoid padding
 	glm::vec4 velocity;
 };
 
@@ -14,26 +14,26 @@ class ParticleSystem
 {
 private:
 	const unsigned int MAX_PARTICLES = 10000;
-	const double SPAWN_RATE_PER_SECOND = 2;
+	const double SPAWN_RATE_PER_SECOND = 6;
 
 	std::shared_ptr<Shader> _computeShader;
 	std::shared_ptr<Shader> _renderShader;
 
-	GLuint _ssbos[2];			// SSBO IDs are in arrays of size 2 because of PingPonging
+	GLuint _ssbos[2];			// Because of ping ponging two SSBOs are needed (1 Input, 1 Output). This also enures having all particles from the previous frame and the current one
 	GLuint _vaos[2];
 	GLuint _atomicCounterID;
 	GLuint _tempBufferID;
 
 	bool _pingPongIndex;		// The currently active SSBO set
 	GLuint _particleCount;		// The number of total particles existent
-	GLuint _readyToSpawn;		// The number of particles that are ready for emission
+	//GLuint _readyToSpawn;		// The number of particles that are ready for emission
 	double _remainingToSpawn;	// The number of particles that are not ready for emission yet
 
 
 	void createAtomicCounter();
 	void createTempBuffer();
-	void addToParticleCount(float deltaTime);
-	void addToActiveSSBOSet(std::vector<Particle> emitters);
+	GLuint getReadyToSpawn(float deltaTime);
+	void addEmittersToActiveSSBO(std::vector<Particle> emitters);
 
 
 public:
@@ -44,6 +44,6 @@ public:
 	void render(glm::mat4 viewMatrix, glm::mat4 projMatrix);
 
 
-	static std::vector<Particle> createEmitters(const unsigned int TTL);
+	static std::vector<Particle> createStarEmitter(const unsigned int TTL);
 	
 };
